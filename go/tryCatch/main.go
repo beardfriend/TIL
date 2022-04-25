18c1b0861721b70a1932f78ab54731f00416132b
beardfriend <beardfriend21@gmail.com>
2022-04-26 06:56:41 +0900

    gin 에러처리 예제 두번째

--- go/tryCatch/main.go
+++ go/tryCatch/main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
-func TestError(param string) (string, error){
+func TestError(param string) (string, error) {
 	if param == "1" {
 		return "1", nil
 	}
-	return "2", errors.New("error")
+	return "2", errors.New("ㅁㄴㅇ")
 }
 
-func Service() string{
+func Service() string {
 	var result string 
 	if val,err := TestError("2"); err !=nil {
-		panic(err)
+		if err.Error() == "error" {
+		panic("레코드가 없습니다")
+	}
+	panic(err)
 	} else {
 		result = val
 	}
@@ -28,15 +32,21 @@ func main() {
 	r.GET("/test", func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
+				str:= fmt.Sprintf("%v",err)
+				if str != "" {
+					c.JSON(500,gin.H{"message": str})	
+					return
+				}
 				c.JSON(500,gin.H{"message": "서버 error"})
 			}
 			}()
-		var result = Service()
 
-		c.JSON(200, gin.H{
+		var result = Service()
+		c.JSON(200, gin.H {
 			"message": result,
 		})
 	
+
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
